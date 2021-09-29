1867465e7d035deff3d10fd7fb174e068a9c44c3
Gabriel <silva.gabriels@hotmail.com>
2021-09-29 21:38:24 +0200

    Update and add new functions to Federation

--- pytolafc/Federation.py
+++ pytolafc/Federation.py
@@ -5,9 +5,41 @@ import numpy as np
 
 class Federation():
     def __init__(self) -> None:
-        self._market_url = "https://api.cartolafc.globo.com/mercado/"
-        self._rounds_url = "https://api.cartolafc.globo.com/rodadas"
-        self._matches_url = "https://api.cartolafc.globo.com/partidas"
+        self._cartola_base_url = "https://api.cartolafc.globo.com/"
+        self._market_status_url = self._cartola_base_url + "mercado/status"
+        self._market_highlights_url = self._cartola_base_url + "mercado/destaques"
+        self._rounds_url = self._cartola_base_url + "rodadas"
+        self._matches_url = self._cartola_base_url + "partidas"
+        self._players_url = self._cartola_base_url + "atletas/mercado"
+        self._clubs_url = self._cartola_base_url + "clubes"
+
+    def request_api_data(sefl, endpoint:str=None):
+        '''
+        Head function to make requests to the Cartola API
+
+        Parameters
+        ----------
+        endpoint : str
+            The endpoint address to make the request
+        
+        Returns
+        -------
+        Dict
+            The data of the request
+
+            Or
+
+            Excpetion in case of response different than status code 200
+        '''
+        if (endpoint):
+            req = requests.get(endpoint)
+
+            if (req.status_code == 200):
+                return (req.json())
+            else:
+                raise Exception("Something went wrong with CartolaFC Official API (x_x)")
+        else:
+            raise Exception("No endpoint passed ! Please provide an endpoint.")
 
     def get_market_status(self) -> dict:
         '''
@@ -22,14 +54,8 @@ class Federation():
         dict
             Dictionary containing the market status response
         '''
-        status_url = self._market_url + "status"
-        req = requests.get(status_url)
-
-        if (req.status_code == 200):
-            return (req.json())
-        else:
-            raise Exception("Something went wrong with CartolaFC Official API (x_x)")
-
+        return self.request_api_data(self._market_status_url)
+        
     def get_market_highlights(self):
         '''
         Get Cartola market highlights data
@@ -37,20 +63,13 @@ class Federation():
         Parameters
         ----------
         param: type
-            
 
         Returns
         -------
         type
             Dictionary containing the market highlights data
         '''
-        status_url = self._market_url + "destaques"
-        req = requests.get(status_url)
-
-        if (req.status_code == 200):
-            return (req.json())
-        else:
-            raise Exception("Something went wrong with CartolaFC Official API (x_x)")
+        return self.request_api_data(self._market_highlights_url)
 
     def get_rounds(self, round:int=None):
         '''
@@ -59,28 +78,59 @@ class Federation():
         Parameters
         ----------
         round: int
-            The number of the round starting from 1
+            The number of the round (starts from 1)
 
         Returns
         -------
         dict
             Dictionary containing the round data
         '''
-        req = requests.get(self._rounds_url)
+        # Get rounds data
+        rounds_data = self.request_api_data(self._rounds_url)
 
-        if (req.status_code == 200):
-            data = req.json()
-
-            if (round > len(data)):
-                print ("Cartola only has {} ! Returning all rounds instead.".format(len(data)))
-                return (data)
+        if (round):
+            if (round > len(rounds_data)):
+                print ("Cartola only has {} ! Returning all rounds instead.".format(len(rounds_data)))
+                return (rounds_data)
             elif (round < 0):
                 print ("Negative round number ! Returning all rounds instead.")
-                return (data)
+                return (rounds_data)
             else:
-                return (data[round-1])
+                return (rounds_data[round-1])
         else:
-            print ("Status code {}".format(req.status_code))
-            raise Exception("Something went wrong with CartolaFC Official API (x_x)")
+            return (rounds_data)
 
-    
+    def get_round_matches(self, round:int=None):
+        '''
+        Get round matches
+
+        Parameters
+        ----------
+        round : int
+            The number of the round to get the matches.
+            If no round number is used, returns the matches for the next round
+
+        Returns
+        -------
+        dict
+            Dictionary with the data for each match
+        '''
+        if (round):
+            return self.request_api_data(self._matches_url + "/{}".format(round))
+        else:
+            return self.request_api_data(self._matches_url)
+
+    def get_players(self):
+        '''
+        Get Cartola players
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        dict
+            Dictionary containing the players data
+        '''
+        return self.request_api_data(self._players_url)
+        
